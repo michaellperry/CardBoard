77498294e0cb069a8a7548460b006f15bc3f623c
Michael L Perry <michael@qedcode.com>
2013-09-20 13:03:20 -0500

    Initial commit.

---
+++ CardBoard/Board/Views/FullScreenBoardControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CardBoard.Board.Views
+{
+    public sealed partial class FullScreenBoardControl : UserControl
+    {
+        public FullScreenBoardControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ToDo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Any())
+            {
+                Doing.SelectedItem = null;
+                Done.SelectedItem = null;
+            }
+        }
+
+        private void Doing_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Any())
+            {
+                ToDo.SelectedItem = null;
+                Done.SelectedItem = null;
+            }
+        }
+
+        private void Done_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Any())
+            {
+                ToDo.SelectedItem = null;
+                Doing.SelectedItem = null;
+            }
+        }
+    }
+}
