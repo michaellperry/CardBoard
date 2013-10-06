c669102664d44a45949aa8de3648df6e83c8c94d
Michael L Perry <michael@qedcode.com>
2013-10-06 13:11:05 -0500

    Use Azure distributor.

--- CardBoard/HTTPConfigurationProvider.cs
+++ CardBoard/HTTPConfigurationProvider.cs
@@ -18,8 +18,8 @@ namespace CardBoard
         {
             get
             {
-                string address = "https://api.facetedworlds.com/correspondence_server_web/bin";
-                string apiKey = "D1920D309A4E43EB85BD14833AFEF7E8";
+                string address = "http://correspondencedistributor.azurewebsites.net/";
+                string apiKey = "Azure";
 				int timeoutSeconds = 30;
                 return new HTTPConfiguration(address, "CardBoard", apiKey, timeoutSeconds);
             }
